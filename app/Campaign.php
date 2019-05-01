48a3d02248cfa0c5f694e405187b47ebfd209d4d
SreyemSOR <sreyemsor@gmail.com>
2019-05-01 09:34:29 +0700

    [S013] crub of campaign

--- app/Campaign.php
+++ app/Campaign.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\TestCase;
 class Campaign extends Model
 {
-    
+    public $fillable = ['name','start_date','end_date','description','status'];
     // one to many relationship between compaign and and testcase
     public function testCases(){
         return $this->hasMany(TestCase::class);
